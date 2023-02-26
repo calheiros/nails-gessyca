91d94adb9292631f5a8e27722fd13c68881b1c4e
calheiros <jo.gj10@gmail.com>
2023-02-26 14:07:25 -0300

    improved style
    added cards example

--- site/js/index.js
+++ site/js/index.js
@@ -1,13 +1,40 @@
 const curses = [
     {
-        name: 'Unhas realistas',
-        url: 'nsme odii',
-        image: 'path to images'
+        name: 'Postiça realistas 2.0',
+        url: 'https://go.hotmart.com/Q80089834O?dp=1',
+        image: 'https://static-media.hotmart.com/cLengJPDbKyAO_caHeKsREdbnf8=/300x300/smart/filters:format(webp):background_color(white)/hotmart/product_pictures/6b51a57d-6b27-46bd-88c8-6eb00d07f1f5/Capa.jpg?w=920'
     },
     {
-        name: 'Teste name',
+        name: 'Como ser uma Irmã melhor',
+        url: ' ',
+        image: 'https://img.freepik.com/fotos-gratis/retrato-de-irmao-e-irma-abracando-e-sorrindo_176420-15678.jpg?w=740&t=st=1677378555~exp=1677379155~hmac=cd1879c56fc28a4c131569f938d03f0687c5d8672c4aa40d16e7f7b43c10a31d'
+    }, 
+    {
+        name: 'como vencer a calvicie',
+        image: 'https://images7.memedroid.com/images/UPLOADED899/588e905ba5f58.jpeg'
+    }
+]
+
+const materials = [
+    {
+        name: 'Top coat',
+        url: ' ',
+        image: 'https://m.media-amazon.com/images/I/41pyDhu9T7L._AC_SL1000_.jpg'
+    },
+    {
+        name: 'Boneca Annabelle',
         url: 'teste urls',
-        image: 'image url'
+        image: 'https://s2.glbimg.com/hmPuJN2Z2Vnpd7cG0EVOw9aAKJ4=/e.glbimg.com/og/ed/f/original/2020/08/14/annabelle-doll-boneca-filme.jpg'
+    },
+    {
+        name: 'Vassoura de Bruxa real',
+        url: ' ',
+        image: 'https://img.elo7.com.br/product/main/1504922/vassoura-de-bruxa-bruxa.jpg'
+    },
+    {
+        name: 'Oleo de Peroba',
+        url: ' ',
+        image: 'https://images.tcdn.com.br/img/img_prod/791033/oleo_de_peroba_king_tradicional_100ml_4813_1_c2f1686a6627345f267da3465cfb34df.jpg'
     }
 ]
 
@@ -16,18 +43,31 @@ function init() {
     let materialCards = document.getElementById("material-cards")
     
     buildCards(curseCards, curses)
-    buildCards(materialCards, curses)
+    buildCards(materialCards, materials)
 }
 
 function buildCards(container, data) {
-    for (let item in data) {
-        let curse = data[item]
-        let title = document.createElement('h2')
+    for (let i in data) {
+        let item = data[i]
+        let card = document.createElement('div')
+        let title = document.createElement('h4')
+        let image = document.createElement('img')
 
-        title.className = 'card'
-        container.append(title)
+        card.className = 'card'
+        card.onclick = function() {
+            window.location.assign(item.url)
+        }
+
+        image.src = item.image
+        image.className = 'card-image'
+    
+        title.innerHTML = item.name
+        title.className = 'card-title'
+        
+        card.append(image)
+        card.append(title)
+        container.append(card)
     }
-    //container.append()
 }
 
 init()
